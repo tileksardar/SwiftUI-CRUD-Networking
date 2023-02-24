c543748eda0f706d2e240608b87ea3d3d0f58838
Your Name <tilek@Tileks-MacBook-Pro.local>
2023-02-24 09:03:16 +0600

    first upload

---
+++ NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Tilek on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+           Home()
+        }
+    }
+}
